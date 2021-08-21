d7b38f7b004ea0b8e76a74845d7d92b12754f9f7
zz-gaurav <56471061+zz-gaurav@users.noreply.github.com>
2021-08-21 12:15:24 +0530

    Add files via upload

---
+++ sudokusolv.py
@@ -0,0 +1,96 @@
+# sudoku solver
+
+# board
+# "0" denotes empty spaces
+board = [
+    [0, 0, 5, 0, 1, 0, 0, 0, 7],
+    [9, 0, 0, 0, 6, 0, 0, 1, 0],
+    [0, 1, 0, 2, 0, 0, 4, 6, 0],
+    [8, 0, 7, 0, 4, 0, 3, 0, 0],
+    [0, 4, 0, 9, 0, 6, 0, 5, 0],
+    [0, 0, 1, 0, 3, 0, 9, 0, 8],
+    [0, 8, 6, 0, 0, 5, 0, 7, 0],
+    [0, 2, 0, 0, 7, 0, 0, 0, 3],
+    [4, 0, 0, 0, 2, 0, 1, 0, 0]
+]
+
+
+# printing board
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("-----------------------------------")
+
+        for j in range(len(bo[0])):
+                if j % 3 == 0 and j != 0:
+                        print(" | ", end=" ")
+
+                if j == 8:
+                        print(bo[i][j])
+                else:
+                        print(str(bo[i][j]) + " ", end=" ")  # row and column
+
+
+print_board(board)
+
+
+#finding empty
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+
+#check valid
+def valid(bo,pos,num):
+        #check row
+        for i in range(len(bo[0])):
+                if bo[pos[0]][i] == num and  pos[1]!=i:
+                    return False
+        #check column
+        for i in range(len(bo)):
+                if bo[i][pos[1]]==num and pos[0]!=i:
+                    return False
+
+        #check box
+
+        box_x = pos[1]//3
+        box_y = pos[0]//3
+
+        for i in range(box_y*3,box_y*3 +3 ):
+                for j in range(box_x*3 , box_x*3+3):
+                        if bo[i][j]==num and bo[i][j]!=pos:
+                                return False
+
+        return True
+
+def solve(bo):
+
+
+        find = find_empty(bo)
+
+        if not find:
+            return True
+        else:
+            row , col = find
+
+        for i in range(1,10):
+            if valid(bo,(row,col),i):
+                bo[row][col]=i
+                if solve(bo):
+                    return True
+
+                bo[row][col]=0
+
+        return False
+
+
+solve(board)
+print("________________________________")
+print("________________________________")
+print_board(board)
